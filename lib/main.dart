41697134fee295b933f9261dd272ca3790c649d0
ben-lawino <lawinwes@gmail.com>
2024-12-23 12:45:40 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies/Home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title: 'Movies',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+      home: Home(),
     );
   }
 }
--- lib/service/http_helper.dart
+++ lib/service/http_helper.dart
@@ -1,8 +1,22 @@
 import 'package:http/http.dart' as http;
+import 'dart:io';
 
-class HttpHelper{
+
+class HttpHelper {
   final String urlKey = 'api_key = f82826bd81afab764a50ad708527178f';
   final String urlBase = 'https://api.themoviedb.org/3/movie';
   final String urlUpcoming = '/upcoming?';
   final String urlLanguage = '&language=en-US';
+
+  Future <String?> getUpcoming() async {
+    final String upcoming = urlBase + urlUpcoming + urlKey + urlLanguage;
+    http.Response result = await http.get(upcoming as Uri);
+
+    if (result.statusCode == HttpStatus.ok) {
+      String responseBody = result.body;
+    }
+    else {
+      return null;
+    }
+  }
 }
