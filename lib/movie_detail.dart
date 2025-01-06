6886399480c497ad6ce156284d6c9f9a9d8da948
ben-lawino <lawinwes@gmail.com>
2025-01-07 01:48:43 +0300

    updated code

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -3,7 +3,7 @@ import 'movie.dart';
 
 class MovieDetail extends StatelessWidget {
   final Movie movie;
-  final String imgPath ='https://image.tmdb.org/t/p/w500/';
+  final String imgPath = 'https://image.tmdb.org/t/p/w500/';
 
   const MovieDetail({super.key, required this.movie});
 
@@ -12,52 +12,92 @@ class MovieDetail extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
 
     String path;
-    if (movie.posterPath != null){
+    if (movie.posterPath != null) {
       path = imgPath + movie.posterPath;
-    }
-    else{
-      path = 'https://image.freeimages.com/images/large_previews/5eb/movie-clapboard-1184339.jpg';
+    } else {
+      path =
+          'https://image.freeimages.com/images/large_previews/5eb/movie-clapboard-1184339.jpg';
     }
     return Scaffold(
       body: Center(
-          child: Column(
-            children: [
-              Container(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)
-                  ), // Match the container's radius
+                    bottomRight:
+                        Radius.circular(40)), // Match the container's radius
+                child: SizedBox(
+                  height: height / 1.4,
                   child: Image.network(
                     path,
-                    fit: BoxFit.fill,
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              SizedBox(height: 15),
-              Text(movie.title,style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-              SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Text(movie.releaseDate,style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text('Release Date',style: TextStyle(fontWeight: FontWeight.w500,color: Colors.black45),)
-                    ],
-                  ),
-                  SizedBox(width: 10,),
-                  Column(
-                    children: [
-                      Text(movie.voteAverage.toString(),style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text('Votes',style: TextStyle(fontWeight: FontWeight.w500,color: Colors.black45),)
-                    ],
-                  ),
-                ],
-              )
-            ],
-          ),
+            ),
+            SizedBox(height: 15),
+            Text(
+              movie.title,
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      movie.releaseDate,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Release Date',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.black45),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      movie.voteAverage.toString(),
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Votes',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.black45),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                  color: Colors.deepOrange, shape: BoxShape.circle),
+              child: GestureDetector(
+                onTap:() {Navigator.of(context).pop();},
+                  child: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              )),
+            )
+          ],
         ),
+      ),
     );
   }
 }
--- lib/service/http_helper.dart
+++ lib/service/http_helper.dart
@@ -1,25 +1,42 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'package:movies/movie.dart';
 
 class HttpHelper {
-  final String urlKey = 'api_key=f82826bd81afab764a50ad708527178f';
+  final String apiKey = 'f82826bd81afab764a50ad708527178f';
   final String urlBase = 'https://api.themoviedb.org/3/movie';
-  final String urlUpcoming = '/upcoming?';
+  final String urlUpcoming = '/upcoming';
   final String urlLanguage = '&language=en-US';
+  final String urlSearchBase = 'https://api.themoviedb.org/3/search/movie';
 
   Future<List?> getUpcoming() async {
-    final String upcoming = urlBase + urlUpcoming + urlKey + urlLanguage;
+    final String upcoming = '$urlBase$urlUpcoming?api_key=$apiKey$urlLanguage';
     final Uri uri = Uri.parse(upcoming);
     http.Response result = await http.get(uri);
 
     if (result.statusCode == HttpStatus.ok) {
       final jsonResponse = jsonDecode(result.body);
       final moviesMap = jsonResponse['results'];
-      List movies =  moviesMap.map((i) =>
-          Movie.fromJson(i)).toList();
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List?> findMovies(String title) async {
+    final Uri uri = Uri.parse(urlSearchBase).replace(queryParameters: {
+      'api_key': apiKey,
+      'query': title,
+    });
+
+    http.Response result = await http.get(uri);
+
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = jsonDecode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
       return movies;
     } else {
       return null;
